88175a5be70637095e6187e021cd9f3a8ff05962
Louis Bompart <louis.vincent.bompart@gmail.com>
2016-02-23 15:23:19 +0100

    Complete test

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -1,11 +1,43 @@
 var assert = require('assert'),
     config = require('./../lib/config');
 
+var units = {
+      thingy: 42,
+      stuffy: 428
+    },
+    ip = '4587';
+
+var Config = config(units, ip);
 
 describe('Config', function () {
-    it('has 3 props')
-
-    it('after construction, 2 props are populated')
-
-    it('has correct key types')
+    it('has 3 props',function () {
+      assert(Object.keys(Config).length === 3);
+    });
+    it('has "units" as prop',function () {
+      assert(Object.keys(Config).indexOf('units')>-1);
+    });
+    it('has "ip" as prop',function () {
+      assert(Object.keys(Config).indexOf('ip')>-1);
+    });
+    it('has "args" as prop',function () {
+      assert(Object.keys(Config).indexOf('args')>-1);
+    });
+    it('after construction, 2 props are populated',function () {
+      var nbPopulated = 0;
+      var props = Object.keys(Config);
+      props.forEach(function (key) {
+        if (key!= null || key != '') {
+          nbPopulated++;
+        }
+      });
+      assert(nbPopulated,2);
+    });
+    it('has correct key types',function () {
+      var isCorrect = true;
+      var props = Object.keys(Config);
+      props.forEach(function (key) {
+        isCorrect = (typeof(key) === 'object' || typeof(key) === 'string');
+      });
+      assert(isCorrect);
+    })
 });
